1598c247d016ac6bb17df4bac05792714a4bda5c
fynn.pritzkau <fynnpritzkau@gmail.com>
2024-01-09 19:50:30 +0100

    Added TemperatureConverter

---
+++ smol is good/src/main/java/TemperatureConverter/TemperatureConverterMain.java
@@ -0,0 +1,37 @@
+package TemperatureConverter;
+
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner;
+
+public class TemperatureConverterMain
+{
+    public static void main(String[] args)
+    {
+        new TemperatureConverterMain();
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public TemperatureConverterMain()
+    {
+        if(scanner.nextInt() == 1)
+        {
+            System.out.println(convertCelsiusToFahrenheit(scanner.nextDouble()));
+        }
+        else
+        {
+            System.out.println(convertFahrenheitToCelsius(scanner.nextDouble()));
+        }
+    }
+
+    private double convertCelsiusToFahrenheit(double celsius)
+    {
+        return celsius * ((double) 9 /5) + 32;
+    }
+
+    private double convertFahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * ((double) 5 /9);
+    }
+}
